0e15d3187724a5ea1b3e42569677e3e0dbc91286
Thomas Garver <tgarv@mit.edu>
2013-04-29 21:51:17 -0400

    Added an example sketch for using the Ethernet shield and making requests.

---
+++ test_ethernet/test_ethernet.ino
@@ -0,0 +1,91 @@
+/*
+  Web client
+ 
+ This sketch connects to a website (http://www.google.com)
+ using an Arduino Wiznet Ethernet shield. 
+ 
+ Circuit:
+ * Ethernet shield attached to pins 10, 11, 12, 13
+ 
+ created 18 Dec 2009
+ modified 9 Apr 2012
+ by David A. Mellis
+ 
+ */
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+// Enter a MAC address for your controller below.
+// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+byte mac[] = {  0x90, 0xA2, 0xDA, 0x00, 0x54, 0xFA };
+IPAddress server(18,111,107,181); // my laptop
+
+// Initialize the Ethernet client library
+// with the IP address and port of the server 
+// that you want to connect to (port 80 is default for HTTP):
+EthernetClient client;
+
+void setup() {
+ // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+   while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  Serial.println("Here");
+
+  // start the Ethernet connection:
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // no point in carrying on, so do nothing forevermore:
+    for(;;)
+      ;
+  }
+  // give the Ethernet shield a second to initialize:
+  delay(1000);
+  Serial.println("connecting...");
+
+  makePostRequest("response_value=<Response>stuff</Response>\n");
+}
+
+// Thanks to http://exosite.com/project/basic-arduino-temperature-web-monitor
+void makePostRequest(String requestContent) {
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 5000)) {
+    Serial.println("connected");
+    // Make a HTTP request:
+    client.print("POST /nfc-response HTTP/1.1\n");
+    client.println("Content-Type: application/x-www-form-urlencoded; charset=utf-8");
+    client.print("Content-Length: ");
+    client.println(requestContent.length());
+    client.println();
+    client.print(requestContent);
+    client.println();
+  } 
+  else {
+    // kf you didn't get a connection to the server:
+    Serial.println("connection failed");
+  }
+}
+
+void loop()
+{
+  // if there are incoming bytes available 
+  // from the server, read them and print them:
+  if (client.available()) {
+    char c = client.read();
+    Serial.print(c);
+  }
+
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+
+    // do nothing forevermore:
+    for(;;)
+      ;
+  }
+}
+
